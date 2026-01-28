a945ef6fc6f044c857e4692e6adaeda29568489b
myloo23 <milan.takacs24@gmail.com>
2026-01-28 13:59:44 +0100

    documentation

--- src/components/shared/feedback/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Shared Feedback Components - Central Export
- */
-
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as ErrorMessage } from './ErrorMessage';
-export { default as SuccessMessage } from './SuccessMessage';
-export { default as FeedbackMessages } from './FeedbackMessages';
